266b8c3c85c3981555cee93f84e973f21e63b380
nandalalainfotech <nandalalainfotech@gmail.com>
2025-02-27 17:53:25 +0530

    changes  in strapi  plugins

--- backend/my-strapi-project/config/plugins.ts
+++ backend/my-strapi-project/config/plugins.ts
@@ -1,22 +1,32 @@
 export default ({ env }) => ({
-    upload: {
-      config: {
-        provider: "@strapi/provider-upload-cloudinary",
-        providerOptions: {
-          cloud_name: env("CLOUDINARY_NAME"),
-          api_key: env("CLOUDINARY_KEY"),
-          api_secret: env("CLOUDINARY_SECRET"),
-        },
-        actionOptions: {
-            upload: {},
-            delete: {},
-          },
+  upload: {
+    config: {
+      provider: "@strapi/provider-upload-cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
   },
   'users-permissions': {
     config: {
       jwtSecret: env('JWT_SECRET', 'QLYgaOtEj+LYiOLojoJ4jw=='),
     },
   },
-  });
-  
+  auth: {
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'),
+    },
+  },
+});
